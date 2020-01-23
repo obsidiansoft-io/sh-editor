038646b860efa0e6ce8ec7af7c9583554a37acbd
Alfredo Medrano Sánchez <alfredomedranosanchez@gmail.com>
2020-01-23 11:53:48 -0600

    add antd

--- app/Screens/Home/index.js
+++ app/Screens/Home/index.js
@@ -1,35 +1,54 @@
 import React, { useState } from 'react';
+import { Button, Select, Row, Col } from 'antd';
 import AceEditor from 'react-ace';
 import parser from '../../Utils/parser';
 
 import 'ace-builds/src-noconflict/mode-json';
+import 'ace-builds/src-noconflict/mode-html';
 import 'ace-builds/src-noconflict/theme-monokai';
 
 function Home(props) {
   const [page, setPage] = useState('');
+  const [mode, setMode] = useState('json');
   function onChange(newValue) {
-    console.log(page);
     setPage(newValue);
   }
-  function createBrowserWindow() {
-    const html = `<body>${parser(JSON.parse(page).content)}</body>`;
-    console.log(html);
+  async function createBrowserWindow() {
+    const parsed = parser(JSON.parse(page).content);
+    const html = `<head><title>Sharabiz Page View</title><meta http-equiv="Content-Security-Policy" content="script-src 'self';"></head><body><div>${parsed.toString()}</div></body>`;
     const { remote } = require('electron');
     const { BrowserWindow } = remote;
     const win = new BrowserWindow({
       height: 600,
       width: 800
     });
-    console.log(html);
     win.loadURL('data:text/html;charset=utf-8,' + encodeURI(html));
   }
   return (
-    <div className="home" style={{ width: '100%', height: '100%' }}>
-      <button onClick={createBrowserWindow}>Test</button>
+    <div className="home" style={{ width: '100%', height: '100vh' }}>
+      <Row type="flex" justify="space-between" style={{ marginBottom: 20 }}>
+        <Col>
+          <span>Mode:</span>
+          <Select
+            style={{ width: 100 }}
+            value={mode}
+            onSelect={e => setMode(e)}
+          >
+            <Select.Option value="json">JSON</Select.Option>
+            <Select.Option value="html">HTML</Select.Option>
+          </Select>
+        </Col>
+        <Col>
+          <Button onClick={createBrowserWindow} type="primary">
+            Run
+          </Button>
+        </Col>
+      </Row>
       <AceEditor
-        mode="json"
+        mode={mode}
         theme="monokai"
         onChange={onChange}
+        value={page}
         name="UNIQUE_ID_OF_DIV"
         editorProps={{ $blockScrolling: true }}
         commands={['children']}
