ba112cb6aacd6e3902efb4eab279529a4d49f3d4
Alfredo Medrano Sánchez <alfredomedranosanchez@gmail.com>
2020-01-23 19:53:46 -0600

    fix link of daata

--- app/Lib/renderView.js
+++ app/Lib/renderView.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 import { remote } from 'electron';
 import parser from '../Utils/parser';
 
-const { BrowserWindow } = remote;
+const { BrowserWindow, app } = remote;
 
 let view = null;
 
@@ -31,20 +31,29 @@ export default async function createViewFrame(mode, page) {
   if (mode === 'json') {
     const parsed = parser(JSON.parse(page).content);
     const html = `<html><head><title>Sharabiz Page View</title><meta http-equiv="Content-Security-Policy" content="script-src 'self';"></head><body>${parsed.toString()}</body></html>`;
-    fs.writeFile(path.join(__dirname, '../cache/view.html'), html, () => {
-      if (!view) {
-        view = new BrowserWindow({
-          show: false,
-          width: 1024,
-          height: 728,
-          webPreferences: {
-            nodeIntegration: true
-          }
-        });
+    if (!fs.existsSync(path.join(app.getPath('appData'), 'sheditor'))) {
+      fs.mkdirSync(path.join(app.getPath('appData'), 'sheditor'));
+    }
+    fs.writeFile(
+      path.join(app.getPath('appData'), 'sheditor', 'view.html'),
+      html,
+      () => {
+        if (!view) {
+          view = new BrowserWindow({
+            show: false,
+            width: 1024,
+            height: 728,
+            webPreferences: {
+              nodeIntegration: true
+            }
+          });
+        }
+        view.loadURL(
+          path.join(app.getPath('appData'), 'sheditor', 'view.html')
+        );
+        view.show();
+        view.focus();
       }
-      view.loadURL(path.join(__dirname, '../cache/view.html'));
-      view.show();
-      view.focus();
-    });
+    );
   }
 }
